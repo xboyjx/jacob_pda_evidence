c4eab68b8ef2b6ce90452e1b8d39494c4a4ca9b2
Jacob <callaghanjacob@gmail.com>
2022-03-20 19:20:47 +0000

    tasks up to date as of 180322. Includes I.T.5, I.T.6, A.D.2, A.D.3

---
+++ I.T.6/dictionaries_PDA.py
@@ -0,0 +1,11 @@
+my_favorite_things = {
+    "Food":"Pizza",
+    "Movie":"Star Wars",
+    "Drink":"Coffee",
+    "Snack":"KitKat"
+}
+
+def display_favorite_movie(favorite_things):
+    print(favorite_things["Movie"])
+
+display_favorite_movie(my_favorite_things)
